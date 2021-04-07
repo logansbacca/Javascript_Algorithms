c00073a5287c2d6b02b940e39029ae4fa6977142
logansbacca <61002844+logansbacca@users.noreply.github.com>
2021-04-07 20:18:49 +0200

    Update Slice and SplicePassed.js

--- Slice and SplicePassed.js
+++ Slice and SplicePassed.js
@@ -1,10 +1,8 @@
 function frankenSplice(arr1, arr2, n) {
- let newArray = arr2.slice();
- for(let i=0; i<arr1.length; i++) {
-   newArray.splice(n,0,arr1[i])
-   n++;
- }
- return newArray
+  let newArr= arr2.slice();
+  newArr.splice(n,0,...arr1)
+  return newArr 
+
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
