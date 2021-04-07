2f85dcb534c3720a0dc3c44ebb98b3dcb7d520e7
logansbacca <61002844+logansbacca@users.noreply.github.com>
2021-04-07 19:37:45 +0200

    Update Return Largest Numbers in ArraysPassed.js

--- Return Largest Numbers in ArraysPassed.js
+++ Return Largest Numbers in ArraysPassed.js
@@ -1,10 +1,11 @@
 function largestOfFour(arr) {
+ var arrNew = [];
+  for(var i = 0; i < arr.length; i++){
+  arrNew.push(arr[i].sort(function(a, b){return b-a})[0]);
 
-    let newArray = []
-    for (let i=0; i<arr.length; i++) {
-    arr[i].sort(function(a, b){return b - a});
-    newArray[i] = arr[i][0];
   }
-    return newArray
+
+ console.log(arrNew);
 }
+
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
